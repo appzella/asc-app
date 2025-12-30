60f1c39a6746e47a3125103ce37ce2377661ee48
appzella <pascalstaub@me.com>
2025-12-30 15:29:30 +0100

    feat: Add animated Collapsible component for sidebar submenus
    Created custom animated CollapsibleContent using motion library:
    - Spring-based height animation (stiffness: 500, damping: 40)
    - Smooth opacity transition during expand/collapse
    - Sidebar submenus now animate smoothly

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,11 +1,44 @@
-"use client"
+'use client'
 
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import * as React from 'react'
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
+import { AnimatePresence, motion } from 'motion/react'
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+// Animated CollapsibleContent with smooth height transition
+const CollapsibleContent = React.forwardRef<
+    React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
+    React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
+>(({ children, className, ...props }, ref) => {
+    return (
+        <CollapsiblePrimitive.CollapsibleContent
+            ref={ref}
+            className={className}
+            {...props}
+            asChild
+            forceMount
+        >
+            <AnimatePresence initial={false}>
+                <motion.div
+                    key="collapsible-content"
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: 'auto', opacity: 1 }}
+                    exit={{ height: 0, opacity: 0 }}
+                    transition={{
+                        height: { type: 'spring', stiffness: 500, damping: 40 },
+                        opacity: { duration: 0.15 }
+                    }}
+                    style={{ overflow: 'hidden' }}
+                >
+                    {children}
+                </motion.div>
+            </AnimatePresence>
+        </CollapsiblePrimitive.CollapsibleContent>
+    )
+})
+CollapsibleContent.displayName = 'CollapsibleContent'
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
