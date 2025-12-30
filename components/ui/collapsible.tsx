4227ef016a6762ab8bcdda2155da5feb557869ef
appzella <pascalstaub@me.com>
2025-12-30 15:39:37 +0100

    fix: Fix Collapsible animation to work with sidebar submenus
    Fixed the Collapsible component to use tailwindcss-animate accordion
    animations (accordion-down/up) instead of broken motion/forceMount approach.
    
    Submenus now:
    - Toggle correctly when clicked
    - Animate smoothly on expand/collapse
    - Don't stay permanently open

--- components/ui/collapsible.tsx
+++ components/ui/collapsible.tsx
@@ -1,44 +1,30 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
-import { AnimatePresence, motion } from 'motion/react'
+import * as React from "react"
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import { cn } from "@/lib/utils"
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-// Animated CollapsibleContent with smooth height transition
+// Animated CollapsibleContent using tailwindcss-animate accordion animations
 const CollapsibleContent = React.forwardRef<
     React.ElementRef<typeof CollapsiblePrimitive.CollapsibleContent>,
     React.ComponentPropsWithoutRef<typeof CollapsiblePrimitive.CollapsibleContent>
->(({ children, className, ...props }, ref) => {
-    return (
-        <CollapsiblePrimitive.CollapsibleContent
-            ref={ref}
-            className={className}
-            {...props}
-            asChild
-            forceMount
-        >
-            <AnimatePresence initial={false}>
-                <motion.div
-                    key="collapsible-content"
-                    initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: 'auto', opacity: 1 }}
-                    exit={{ height: 0, opacity: 0 }}
-                    transition={{
-                        height: { type: 'spring', stiffness: 500, damping: 40 },
-                        opacity: { duration: 0.15 }
-                    }}
-                    style={{ overflow: 'hidden' }}
-                >
-                    {children}
-                </motion.div>
-            </AnimatePresence>
-        </CollapsiblePrimitive.CollapsibleContent>
-    )
-})
-CollapsibleContent.displayName = 'CollapsibleContent'
+>(({ className, children, ...props }, ref) => (
+    <CollapsiblePrimitive.CollapsibleContent
+        ref={ref}
+        className={cn(
+            "overflow-hidden",
+            "data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up",
+            className
+        )}
+        {...props}
+    >
+        {children}
+    </CollapsiblePrimitive.CollapsibleContent>
+))
+CollapsibleContent.displayName = "CollapsibleContent"
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
